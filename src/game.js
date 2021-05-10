4bc03d8c729a157d86565c4ac7e05b66590acf93
Sandra <sandra.luengo@sngular.com>
2021-05-10 20:46:30 +0200

    feat: testing draw

--- src/game.js
+++ src/game.js
@@ -1,10 +1,10 @@
 window.onload = () => {
-  init();
+  start();
 };
 
 let canvas;
 
-let init = () => {
+let start = () => {
   canvas = document.getElementById("game");
   let ctx = canvas.getContext("2d");
   canvas.ctx = ctx;
@@ -13,12 +13,22 @@ let init = () => {
   canvas.canMove = false;
   canvas.mouseArray = [];
   canvas.pos = { x: 0, y: 0 };
+  canvas.fps = 60;
+  canvas.count = 0;
+  canvas.intervalId = null;
   canvas.addEventListener("mousedown", mouseDown);
   canvas.addEventListener("mousemove", mouseMove);
   canvas.addEventListener("mouseup", mouseUp);
 };
 
+let init = () => {
+  canvas.idInterval = setInterval(() => {
+    canvas.count++;
+  }, 1000 / this.fps);
+};
+
 let mouseDown = (e) => {
+  init();
   canvas.canMove = true;
   setPosition(e);
 };
@@ -32,11 +42,11 @@ let setPosition = (e) => {
 let mouseMove = (e) => {
   let { canMove, mouseArray } = canvas;
   if (!canMove) return;
-  mouseArray.push([e.clientX, e.clientY]);
-  if (mouseArray.length > 20) {
-    draw(e);
-    checkFigure();
+  if (canvas.count % 5 === 0) {
+    mouseArray.push({x:e.clientX, y:e.clientY});
   }
+  draw(e);
+  checkFigure();
 };
 
 let draw = (e) => {
@@ -51,14 +61,36 @@ let draw = (e) => {
   ctx.stroke();
 };
 
-
 let checkFigure = () => {
   let { mouseArray } = canvas;
-  console.log(mouseArray);
+  if (!!mouseArray[mouseArray.length-5] && !!mouseArray[mouseArray.length-1]) {
+    let p0 = mouseArray[mouseArray.length-5];
+    let p6 = mouseArray[mouseArray.length-1];
+    let diffX = p6.x - p0.x;
+    let diffY = p6.y - p0.y;
+    console.log(diffX, diffY);
+    if(diffY < 5 && diffY > 0) {
+      console.log('Recta Horizontal')
+    } else {
+      console.log('Otra cosa')
+    }
+    // if (diffX < 5) {
+    //   console.log('Recta Vertical');
+    // } 
+   
+  }
 };
 
 let mouseUp = () => {
   let { ctx } = canvas;
   canvas.canMove = false;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  clearInterval(canvas.idInterval);
+  reset();
+};
+
+let reset = () => {
+  canvas.mouseArray.length = 0;
+  canvas.count = 0;
+  canvas.pos = { x: 0, y: 0 };
 };
