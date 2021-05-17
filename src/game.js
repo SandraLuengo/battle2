f6ffcc04555b7fc1a5f625918e134b7ac81778da
Luengo Chozas <luchosandra@gmail.com>
2021-05-17 21:21:07 +0200

    feat: color change

--- src/game.js
+++ src/game.js
@@ -43,7 +43,7 @@ let mouseMove = (e) => {
   let { canMove, mouseArray } = canvas;
   if (!canMove) return;
   if (canvas.count % 5 === 0) {
-    mouseArray.push({x:e.clientX, y:e.clientY});
+    mouseArray.push({ x: e.clientX, y: e.clientY });
   }
   draw(e);
   checkFigure();
@@ -55,6 +55,8 @@ let draw = (e) => {
   ctx.lineWidth = 7;
   ctx.lineCap = "round";
   ctx.strokeStyle = "white";
+  ctx.shadowColor = 'white';
+  ctx.shadowBlur = 5;
   ctx.moveTo(pos.x, pos.y);
   setPosition(e);
   ctx.lineTo(pos.x, pos.y);
@@ -63,27 +65,46 @@ let draw = (e) => {
 
 let checkFigure = () => {
   let { mouseArray } = canvas;
-  if (!!mouseArray[mouseArray.length-5] && !!mouseArray[mouseArray.length-1]) {
-    let p0 = mouseArray[mouseArray.length-5];
-    let p6 = mouseArray[mouseArray.length-1];
+  if (!!mouseArray[mouseArray.length - 5] && !!mouseArray[mouseArray.length - 1]) {
+    let p0 = mouseArray[mouseArray.length - 5];
+    let p6 = mouseArray[mouseArray.length - 1];
     let diffX = p6.x - p0.x;
     let diffY = p6.y - p0.y;
-    console.log(diffX, diffY);
-    if(diffY < 5 && diffY > 0) {
-      console.log('Recta Horizontal')
+    console.log(p6, p0)
+    if (diffY < 10 && diffY > -5) {
+      changeColor('blue')
+      return 'horizontal'
+    } else if (diffX < 10 && diffX > -5) {
+      changeColor('red') //vertical
+      return 'vertical'
     } else {
-      console.log('Otra cosa')
+      changeColor('white') // v invertida
+      return 'other'
     }
-    // if (diffX < 5) {
-    //   console.log('Recta Vertical');
-    // } 
-   
   }
 };
+
+let changeColor = (color) => {
+  let { mouseArray, ctx } = canvas;
+  ctx.beginPath();
+  ctx.strokeStyle = color;
+  ctx.shadowBlur = 0;
+  ctx.moveTo(mouseArray[0].x, mouseArray[0].y);
+  mouseArray.forEach(function (currentValues) {
+    ctx.lineTo(currentValues.x, currentValues.y);
+  });
+  ctx.stroke();
+  ctx.closePath();
+}
+
+let getFigure = () => {
+  return checkFigure();
+}
 
 let mouseUp = () => {
   let { ctx } = canvas;
   canvas.canMove = false;
+ // getFigure();
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   clearInterval(canvas.idInterval);
   reset();
